4f8149d5ed0ca78ddf20160402e176fd61d52906
davstern <hello@davidstern.me>
2020-01-26 20:59:45 +0100

    chore: codeworks style linter + linting

--- extension/background.js
+++ extension/background.js
@@ -6,55 +6,55 @@ const SERVER_URL = 'http://localhost:3004';
 
 const userId = 1;
 
-chrome.runtime.onConnect.addListener( (port) => {
+chrome.runtime.onConnect.addListener((port) => {
   port.onMessage.addListener((msg,sender) => {
   // whenever a new url is loaded into a tab
-    if(msg.pageData){
+    if (msg.pageData) {
       return onNewUrl(msg.pageData,sender.sender.tab.id);
     }
     // Else this is a timer order
-    return toggleIdle(sender.sender.tab.id,msg.idle)
+    return toggleIdle(sender.sender.tab.id,msg.idle);
   });
 });
 
 const toggleIdle = (tabId,idle)=>{
-  log(`Toggle idle called with argument ${idle}`,'ua')
+  log(`Toggle idle called with argument ${idle}`,'ua');
   const tab = openedTabs[tabId];
-  if(!tab){
-    log('No found tab','e')
+  if (!tab) {
+    log('No found tab','e');
   }
 
-  if(!tab.startTime){
+  if (!tab.startTime) {
     tab.startTime = tab.timeStopped = + new Date();
     tab.isIdle = false;
-    log(['Tab active for the first time ',tab.fullTitle],'ua')
+    log(['Tab active for the first time ',tab.fullTitle],'ua');
     return;
   }
-  if(idle){
+  if (idle) {
     tab.isIdle = true;
-    log(['Tab idle',tab.fullTitle],'ua') 
+    log(['Tab idle',tab.fullTitle],'ua'); 
     tab.timeSpent += + new Date() - tab.timeStopped;
     tab.timeStopped = + new Date();
-  }else{
+  } else {
     tab.isIdle = false;
-    log(['Tab active',tab.fullTitle],'ua') 
+    log(['Tab active',tab.fullTitle],'ua'); 
   }
   
-}
+};
 
 const onNewUrl = (pageData,tabId)=>{  
   log('New page visited 👇 ','ua');
   // Could there be an edgecase ?
-  if(!pageData || !(pageData.fullUrl || pageData.fullTitle)){
+  if (!pageData || !(pageData.fullUrl || pageData.fullTitle)) {
     log('Incomplete pageData passed to background.js','e');
     return;
   }
-  log(pageData,'table')
-  if(openedTabs[tabId]){
+  log(pageData,'table');
+  if (openedTabs[tabId]) {
     log('Tab content replaced','ua');
-    if(openedTabs[tabId].fullUrl !== pageData.fullUrl){
+    if (openedTabs[tabId].fullUrl !== pageData.fullUrl) {
       sendPageData(openedTabs[tabId]);
-    }else{
+    } else {
       log('Refresh','ua');
       return;
     }
@@ -63,66 +63,63 @@ const onNewUrl = (pageData,tabId)=>{
   pageData.tabId = tabId;
   pageData.timeSpent = 0;
   openedTabs[tabId] = pageData; 
-}
+};
 
 chrome.tabs.onRemoved.addListener((tabId) =>{
-  log('Tab removed','ua')
-  if(!openedTabs[tabId]){
-    log('No url found','w')
+  log('Tab removed','ua');
+  if (!openedTabs[tabId]) {
+    log('No url found','w');
     return; // tab with no url, such as widget
   }
   
   sendPageData(openedTabs[tabId]).then((res)=>{
-    delete openedTabs[tabId] ;
+    delete openedTabs[tabId];
   }).catch(e => log(e,'error','e'));
 });
 
 const sendPageData = async (pageData) => {
   log('Sending data to server 👇 ','sa');
-  if(!pageData.isIdle && pageData.timeSpent !== 0){
+  if (!pageData.isIdle && pageData.timeSpent !== 0) {
     pageData.timeSpent += + new Date() - pageData.timeStopped;
   }
   delete pageData.isIdle;
   delete pageData.timeStopped;
 
-  log(pageData,'table')
+  log(pageData,'table');
   const response = await fetch(SERVER_URL,{
-    method: 'POST',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
-    headers: {
-      'Content-Type': 'application/json',
+    method : 'POST',
+    mode : 'cors',
+    cache : 'no-cache',
+    credentials : 'same-origin',
+    headers : {
+      'Content-Type' : 'application/json',
     },
-    body: JSON.stringify(pageData), // body data type must match "Content-Type" header
+    body : JSON.stringify(pageData), // body data type must match "Content-Type" header
   });
-  if(!(response.status === 201 && response.ok)){
+  if (!(response.status === 201 && response.ok)) {
     log(['Response status',response.status],'sa');
-   
   }
 };
 
-const log = (input,consoleFunc='log') => {
+const log = (input,consoleFunc = 'log') => {
   const logStyles = {
-    ua:{style:'color: Chocolate;',before:'🚶'}, // user action
-    sa:{style:'color: lightblue',before:'🚀 '}, // software action
-    w: {style:'color: yellow',before:'⚠️'},
-    e:{style:'color: red;font-weight:bold',before:'💩'},
-    log:{style:'',before:''}
-  }
-  switch(consoleFunc){
+    ua : {style : 'color: Chocolate;',before : '🚶'}, // user action
+    sa : {style : 'color: lightblue',before : '🚀 '}, // software action
+    w : {style : 'color: yellow',before : '⚠️'},
+    e : {style : 'color: red;font-weight:bold',before : '💩'},
+    log : {style : '',before : ''}
+  };
+  switch (consoleFunc) {
   case 'table':
-    console.table(input);
+    console.table(input); 
     break;
   default:
-    if(!logStyles[consoleFunc])consoleFunc = 'log'
-    if(typeof input === 'object'){
-      console.log(`%c ${logStyles[consoleFunc].before} ${input[0]} : %c${input[1]} `,logStyles[consoleFunc].style,logStyles[consoleFunc].style+'font-style:italic;');
-    }else{
+    if (!logStyles[consoleFunc])consoleFunc = 'log';
+    if (typeof input === 'object') {
+      console.log(`%c ${logStyles[consoleFunc].before} ${input[0]} : %c${input[1]} `,logStyles[consoleFunc].style,logStyles[consoleFunc].style + 'font-style:italic;');
+    } else {
       console.log(`%c ${logStyles[consoleFunc].before} ${input}`,logStyles[consoleFunc].style);
     }
     break;
-  }
-  
-}
-
+  } 
+};
--- extension/content.js
+++ extension/content.js
@@ -2,40 +2,40 @@ let idle = true;
 let SCRIPT_KILLED = false;
 let timer;
 
-if(!(!document.URL || document.URL.startsWith('chrome://'))){
+if (!(!document.URL || document.URL.startsWith('chrome://'))) {
   const pageData = {
     fullUrl : document.URL,
-    fullTitle: document.title,
-    text: document.body.innerText.replace(/^(\s*\r\n){2,}/,'\r\n'),
+    fullTitle : document.title,
+    text : document.body.innerText.replace(/^(\s*\r\n){2,}/,'\r\n'),
   };
 
-  const port = chrome.runtime.connect({name: 'knockknock'});
+  const port = chrome.runtime.connect({name : 'knockknock'});
     
   const resetIdleTimer = ()=> {
-    if(SCRIPT_KILLED)return;
+    if (SCRIPT_KILLED) return;
     clearTimeout(timer);
-    if(idle){
+    if (idle) {
       idle = false;
       port.postMessage({idle});
     }
-    timer = setTimeout(setIdle,3000)
-  }
+    timer = setTimeout(setIdle,3000);
+  };
   const setIdle = () => {
-    if(SCRIPT_KILLED)return;
+    if (SCRIPT_KILLED) return;
     idle = true;
     port.postMessage({idle});
-  }
- 
+  };
+  
   port.postMessage({pageData});
   document.body.addEventListener('mousemove',resetIdleTimer);
   document.addEventListener('scroll',resetIdleTimer);
-
+  
   port.onDisconnect.addListener(()=>{
-    console.log('disconnected from background script')
+    console.log('disconnected from background script');
     document.body.removeEventListener('mousemove',resetIdleTimer);
     document.removeEventListener('scroll',resetIdleTimer);
     SCRIPT_KILLED = true;
-  })
-    
+  });
+  
 }
 
