b6fb2d73ab4f5872c02d47e9c3a2223e102240f8
Buzzaw91 <aleksi.ylonen2@gmail.com>
2020-01-28 18:48:58 +0100

    feat: togglebutton check

--- extension/content.js
+++ extension/content.js
@@ -1,8 +1,54 @@
 let idle = true;
 let SCRIPT_KILLED = false;
 let timer;
+let $ = require('jQuery');
+let value = 'ENABLE';
+let state = {};
 
 console.log('Hello!');
+
+const initState = async () => {
+  if (typeof state.active === 'undefined') {
+    await chrome.storage.local.set({active: true}, () => {
+      state.active = true;
+      console.log('activated!');
+    });
+  }
+  else {
+    return console.log('state exists');
+  }
+};
+
+const getState = async () => {
+  await chrome.storage.local.get(['active'], (result) => {
+    console.log('result.active: ', result.active);
+    console.log('state.active: ', state.active);
+  });
+}
+
+const toggleState = async (val) => {
+  if (val === 'ENABLE') {
+    await chrome.storage.local.set({active: false}, () => {
+      state.active = false;
+      console.log('de-activated!');
+      console.log(state.active);
+    });
+  }
+   else if (val === 'DISABLE') {
+    await chrome.storage.local.set({active: true}, () => {
+      state.active = true;
+      console.log('re-activated!');
+      console.log(state.active);
+    });
+   }
+}
+
+
+
+
+
+
+
 
   // Rest goes here...
   if (!(!document.URL || document.URL.startsWith('chrome://'))) {
