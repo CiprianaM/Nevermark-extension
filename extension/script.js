6e2cb4fbe1aafd818d079fef89e46feac3f3fa69
Buzzaw91 <aleksi.ylonen2@gmail.com>
2020-02-03 12:02:02 +0100

    feat: regex & encodeURI work

--- extension/script.js
+++ extension/script.js
@@ -1,20 +1,49 @@
+const SERVER_URL = 'http://192.168.1.243:3000';
+const DEFAULT_ROUTE = '/';
+
+
+const queryApi = async (search) => {
+
+  try {
+    search = encodeURI(search);
+    const res = await fetch(SERVER_URL + DEFAULT_ROUTE + search);
+    alert(res);
+    return res;
+  }
+  catch (err) {
+    console.log(err);
+  }
+}
+
+
+
 window.onload = () => {
   let regex = /q={1}(\S+?)&/;
-  let paragraph = window.location.href;
-  let result = paragraph.match(regex);
+  let url = window.location.href;
+  url = decodeURI(url);
+  let result = url.match(regex);
 
-  alert(result[1]);
-}
+  let testQuery = queryApi(result[1].replace('+', ' '));
+
+
+ /* let queryDiv = queryApi(result[1])
+    .then(res => res.json())
+    .then (res.results)
+    return res.results;
+ */
+
+
+
 
 // STEPS
 // 1st: The script should grab the value of the input field... somehow...
-// 2nd:
+//  replace the + character with %20 for a space for better search results
 
-
+}
 
 let newDiv = document.createElement('div');
 newDiv.id = 'newId';
-newDiv.style.color = 'red';
+newDiv.style.textAlign = 'center';
 let newText = document.createTextNode('Hello There!');
 newDiv.appendChild(newText);
 
